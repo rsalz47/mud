a1e6d446b35d6049f237ede6e108dd8500258899
rsalz47 <ronan.salz@gmail.com>
2021-11-06 02:21:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Museum of User Design
         </p>
 
         <textarea id="searchfield" name="searchfield" rows="1" columns="3">
