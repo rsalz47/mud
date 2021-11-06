adc5681fe79d775faae2a7380fb3b650678c0f11
ewu2023 <ericwu2019@gmail.com>
2021-11-06 17:56:51 -0400

    API Skeleton
    Added skeleton API to server index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,25 @@ app.get("/", (req, res)=>{
     res.json("hello world!")
 })
 
+// Skeleton for database and image API
+app.get("/getImages", (req, res) => {
+    // Take in coordinates
+    // Pass to database 
+    res.end("Images received");
+})
+
+app.post("/insertImage", (req, res) => {
+    // Save image somehow
+    // Send to database
+    res.end("Image uploaded successfully!");
+})
+
+app.get("/getByCoordinate/x=:x/y=:y", (req, res) => {
+    // Get image from database at (x,y)
+    // Send data to front end
+    res.end(`Image found at (${req.params.x}, ${req.params.y})`);
+})
+
 //when hosted on VPS, change this to listen on every port?
 //Essentially, wait for anything to talk to us
 app.listen(3001)
