ef13cbac6eae76a3a5d692779efaf1357f841cf3
rsalz47 <ronan.salz@gmail.com>
2021-11-06 03:55:23 -0400

    created API on server, basic communication from client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+
+  //I do not know what this code does, plz explain Jack
+  //Some guy came and slapped this shit down, and now it just works
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    //this port will be changed to wherever the VPS is hosted
+    fetch("http://localhost:3001")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  //return val is just the HTML/CSS of the webpage itself
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Welcome to the Museum of User Design
-        </p>
+
+        <p>Welcome to the Museum of User Design</p>
 
         <textarea id="searchfield" name="searchfield" rows="1" columns="3">
           x,y
         </textarea>
         <p></p>
-        <button type="button">
-          Visit room (x,y)
-        </button>
-      
+        <button type="button">Visit room (x,y)</button>
+        {data}
       </header>
     </div>
   );
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const morgan = require("morgan")
+
+const app = express()
+
+const cors = require("cors")
+
+app.use(cors())
+app.use(morgan("dev"))
+app.get("/", (req, res)=>{
+    res.json("hello world!")
+})
+
+//when hosted on VPS, change this to listen on every port?
+//Essentially, wait for anything to talk to us
+app.listen(3001)
