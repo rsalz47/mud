52bb956164cc3fa8193e8c48913a8bb71f3b0de8
Jack Bisceglia <57922742+jackbisceglia@users.noreply.github.com>
2021-11-06 17:55:41 -0400

    Add db config (#3)
    * add database connection
    
    * test db connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const express = require("express")
+const { Pool } = require('pg')
 const morgan = require("morgan")
+const { DB_CONNECTION, DB_PASSWORD} = require('./SECRETS')
 
+console.log(DB_CONNECTION, DB_PASSWORD)
 const app = express()
 
 const cors = require("cors")
+
+const pool = new Pool({
+    host: DB_CONNECTION,
+    user: 'postgres',
+    password: DB_PASSWORD,
+    port: 5432,
+    database: 'postgres',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
 
 app.use(cors())
 app.use(morgan("dev"))
 
 app.get("/", (req, res)=>{
     res.json("hello world!")
+    pool.query()
 })
 
 //when hosted on VPS, change this to listen on every port?
