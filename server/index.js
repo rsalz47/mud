ad2026313a18129f21899cccb07ef6f361bdac1c
rsalz47 <ronan.salz@gmail.com>
2021-11-06 21:06:56 -0400

    'smart' search based on name
    Added rudimentary 'intelligent' searching, by performing trivial character comparisons and choosing which element has the highest score

--- server/index.js
+++ server/index.js
@@ -58,20 +58,91 @@ app.get("/getByCoordinate/x=:x/y=:y", (req, res) => {
     let rows = [];
     pool.query(`SELECT * FROM Museum 
                 WHERE x = ${req.params.x} AND y = ${req.params.y}`, 
-        (err, result) => {
-        if (err) {
-            return console.error('Error executing query', err.stack)
-        }
-        console.log(result.rows)
-        //if not empty populate return val
-        if (result.rows !== []) {
-            rows = result.rows
-        }
-        //pipe to front end
-        res.json(rows);
-      })
+                (err, result) => {
+                    if (err) {
+                        return console.error('Error executing query', err.stack)
+                    }
+                    console.log(result.rows)
+                    //if not empty populate return val
+                    if (result.rows !== []) {
+                        rows = result.rows
+                    }
+                    //pipe to front end
+                    res.json(rows);
+                }
+    )
 })
 
+app.get("/getByName/name=:name", (req, res) => {
+    let rows = []
+    let name = req.params.name.toLowerCase()
+    let bestname = getBestName()
+    
+    pool.query(`SELECT * FROM Museum 
+                WHERE name = ${bestname}`,   
+                (err,result) => {
+                    if (err) {
+                        return console.error('Error executing query', err.stack)
+                    }
+                    if (result.rows !== []) {
+                        rows = result.rows
+                    }
+                    res.json(rows)
+                }
+    )
+})
+
+function getBestName(name) {
+    let allNames = []
+
+    pool.query(`SELECT name FROM Museum`,   
+                (err,result) => {
+                    if (err) {
+                        return console.error('Error executing query', err.stack)
+                    }
+                    if (result.rows !== []) {
+                        allNames = result.rows
+                    }
+                }
+    )
+
+    if (allNames === []) {
+        return name;
+    }
+
+    let allNamesScores = Array.new(allnames.length) 
+    allNames.forEach((elem) => elem.toLowerCase())
+
+    //calculate the score for every name
+    //score is incremented if characters are in the same position
+    //there's absolutely ways to improve, like maybe check adjacent indices to see if 
+    //letters are "close enough"
+    allNames.forEach((elem, index) => {
+        let similarityScore = 0;
+        let currInd = elem.length - 1;
+        if (currInd > name.length - 1) {
+            score -= (currInd - name.length - 1)
+            currInd = name.length
+        }
+        if (currInd < name.length - 1) {
+            score -= (name.length - 1 - currInd)
+        }
+
+        for(; currInd > -1; currInd ++) {
+            if (elem.charAt(currInd) === name.charAt(currInd)) {
+                score++;
+            }
+        }
+
+        allNamesScores[index] = similarityScore;
+    })
+    let bestInd = 0;
+    allNames.forEach((elem, ind) => {
+        if(allNamesScore[ind] > allNamesScores[bestInd])
+            bestInd = ind
+    })
+    return allNames[ind]
+}
 //when hosted on VPS, change this to listen on every port?
 //Essentially, wait for anything to talk to us
 app.listen(3001)
